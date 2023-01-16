e96b9fd271257ca78e032a86362109faeb6dc3b3
Chi Wang <chiwang@google.com>
2023-01-16 16:46:16 +0100

    Test bazelci-agent aarch64 linux

--- buildkite/bazelci.py
+++ buildkite/bazelci.py
@@ -1642,11 +1642,14 @@ def download_bazelci_agent(dest_dir, version):
         else:
             postfix = "x86_64-apple-darwin"
     else:
-        postfix = "x86_64-unknown-linux-musl"
+        if platform_module.machine() == "arm64":
+            postfix = "aarch64-unknown-linux-musl"
+        else:
+            postfix = "x86_64-unknown-linux-musl"
 
     name = "bazelci-agent-{}-{}".format(version, postfix)
     url = (
-        "https://github.com/bazelbuild/continuous-integration/releases/download/agent-{}/{}".format(
+        "https://github.com/coeuvre/continuous-integration/releases/download/agent-{}/{}".format(
             version, name
         )
     )
@@ -2202,7 +2205,7 @@ def execute_bazel_coverage(bazel_version, bazel_binary, platform, flags, targets
 
 
 def upload_test_logs_from_bep(bep_file, tmpdir, monitor_flaky_tests):
-    bazelci_agent_binary = download_bazelci_agent(tmpdir, "0.1.3")
+    bazelci_agent_binary = download_bazelci_agent(tmpdir, "0.1.5")
     execute_command(
         [
             bazelci_agent_binary,
